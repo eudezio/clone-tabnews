72dd909702f8896a58d1afcc76d9bb527f77f0e9
Carlos Eudezio da Silva Rosa <carlos_eudezio@yahoo.com.br>
2023-09-25 21:37:44 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Aí óh! Tá funcionando on-line, Fulano</h1>
+function Home() {
+  return <h1>Aí óh! Tá funcionando on-line, Fulano</h1>;
+}
+
+function teste2() {
+  console.log("indentacao certa");
 }
 
 export default Home;
